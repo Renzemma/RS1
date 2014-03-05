eab90e807da5c7192852116decca08ba0b7b1dcb
Renzemma <renzemma@hotmail.com>
2014-03-04 23:31:12 -0300

    client

---
+++ Proyectos/RS1/client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+  {
+    title: 'Who Is RS',
+    author: 'RS',
+    url: 'http://rselectronic.com.ar'
+  }, 
+  {
+    title: 'RS',
+    author: 'RS',
+    url: 'http://rselectronic.com.ar'
+  }, 
+  {
+    title: 'The RS Book',
+    author: 'RS',
+    url: 'http://rselectronic.com.ar'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
